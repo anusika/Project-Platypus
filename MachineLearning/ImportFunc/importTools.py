b98ff1ee53ef7b9ffce4eb3509f6321aec4a6439
anusika <anusikanijher@gmail.com>
2018-01-13 22:36:36 -0800

    Buffering

--- MachineLearning/ImportFunc/importTools.py
+++ MachineLearning/ImportFunc/importTools.py
@@ -1,5 +1,5 @@
 import numpy as numpy
-
+import importHQ as HQ
 #imports file and splits into array
 def importFile(filename,delimiter):
 	results = [];
@@ -9,3 +9,10 @@ def importFile(filename,delimiter):
 		results.append(line.strip(delimiter).split())
 		#print(str(results[len(results)-1]))
 	return results
+
+def buffer():
+        signs = HQ.importAll(signs)
+        maxFrames = HQ.main(signs)
+        return maxFrames
+
+print(buffer())
