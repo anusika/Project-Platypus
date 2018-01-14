67069870e1d9778dae7ca3499b1f2e0029dd0719
Wesley Hong <hong.wesley609@gmail.com>
2018-01-13 18:05:36 -0800

    count down and on space submitt

--- Client.py
+++ Client.py
@@ -1,30 +1,59 @@
  #!/usr/bin/env python
  
 import socket  
+import multiprocessing as mp
+import time
+import sys
+import tty
+import termios
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-# main 
+print "Welcome to Project Platypus"
+print "---------------------------"
+print "Press any key to start recording"
 
+def get_ch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
+def worker():
+    while True:
+        time.sleep(2)
 
+def countDownFrom3():
+    print "It will record in 3"
+    time.sleep(1)
+    print "It will record in 2"
+    time.sleep(1)
+    print "It will record in 1"
+    time.sleep(1)
+    print "Recording...",
 
-
-
-
-
-
+p = mp.Process(target=worker)
+p.start()
+get_ch()
 # once you finish recording 
+countDownFrom3()
+
+# get data and send it 
+
 s.send(MESSAGE)
-
-
 ## when you recieve something 
 data = s.recv(BUFFER_SIZE)
 
 print "received data:", data
+
+# terminate worker thread
+p.terminate()
